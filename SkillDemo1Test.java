0ff165c476b4f8418a0fa2005f2ab78cb8772950
cmy0357 <mic015@ucsd.edu>
2022-04-25 18:59:56 -0700

    Update SkillDemo1Test.java

--- SkillDemo1Test.java
+++ SkillDemo1Test.java
@@ -5,7 +5,7 @@ public class SkillDemo1Test {
     
     @Test
     public void test1() {
-        assertEquals(8, SkillDemo1.multiply(2, 3));
+        assertEquals(8, SkillDemo1.multiply(2, 4));
         // expected to fail
     }
 
